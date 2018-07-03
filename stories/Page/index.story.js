abfc1a2b5e55b0826b13f678167d69e1c9b0534c
Erez Makavy <erez@wix.com>
2018-07-03 16:30:11 +0300

    Add e2e snapshots for FixedContent

--- stories/Page/index.story.js
+++ stories/Page/index.story.js
@@ -92,7 +92,8 @@ export default {
   examples: (
     <div>
       <h1>Test Examples</h1>
-      <h2>With Background-Image, Children: Header + Content (Without Tail)</h2>
+
+      <h2>1) With Background-Image, Children: Header + Content (Without Tail)</h2>
       <PageContainer>
         <Page
           dataHook="story-page-background-image-header-content"
@@ -100,11 +101,30 @@ export default {
           backgroundImageUrl="https://static.wixstatic.com/media/f0548921c53940ec803dfb1c203e96fe.jpg/v1/fill/w_400,h_100/f0548921c53940ec803dfb1c203e96fe.jpg"
           />
       </PageContainer>
-      <h2>With Gradient, Children: Header + Content (Without Tail)</h2>
+
+      <h2>2) With Gradient, Children: Header + Content (Without Tail)</h2>
       <PageContainer>
         <Page
           dataHook="story-page-gradient-header-content"
           children={[header(Breadcrumbs), content(false)]}
+          gradientClassName="background-gradient"
+          />
+      </PageContainer>
+
+      <h2>3) With Background-Image, Children: Header + FixedContent + Content (Without Tail)</h2>
+      <PageContainer>
+        <Page
+          dataHook="story-page-background-image-header-fixed-content-content"
+          children={[header(Breadcrumbs), fixedContent(), content(false)]}
+          backgroundImageUrl="https://static.wixstatic.com/media/f0548921c53940ec803dfb1c203e96fe.jpg/v1/fill/w_400,h_100/f0548921c53940ec803dfb1c203e96fe.jpg"
+          />
+      </PageContainer>
+
+      <h2>4) With Gradient, Children: Header + FixedContent + Content (Without Tail)</h2>
+      <PageContainer>
+        <Page
+          dataHook="story-page-gradient-header-fixed-content-content"
+          children={[header(Breadcrumbs), fixedContent(), content(false)]}
           gradientClassName="background-gradient"
           />
       </PageContainer>
